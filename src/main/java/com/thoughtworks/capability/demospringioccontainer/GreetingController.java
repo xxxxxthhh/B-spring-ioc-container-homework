fe1b0f6556e0c8ffb71a34483836435c1397ac72
KyleXu <kyle_x@foxmail.com>
2020-12-03 17:21:01 +0800

    [XTH]: finished problem 2 using application context

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,31 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private GreetingService greetingService;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    private ApplicationContext applicationContext;
+
+    // @Autowired
+    // public GreetingController(GreetingService greetingService) {
+    //     this.greetingService = greetingService;
+    // }
 
     @GetMapping("/greet")
     public String greet() {
+        greetingService = applicationContext.getBean(GreetingService.class);
         return greetingService.sayHi();
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+      this.applicationContext = applicationContext;
     }
 
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,12 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype")
 public class GreetingService {
-
-    String sayHi() {
-        return "hello world";
-    }
+  String sayHi() {
+    return "hello world";
+  }
 }
